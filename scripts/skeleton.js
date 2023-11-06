d89828ee79503f5af15ec2a1ecf5ab3c4436adca
Caroline Su <caroline77899@gmail.com>
2023-11-05 18:03:43 -0800

    fix infinite login loop

---
+++ scripts/skeleton.js
@@ -0,0 +1,19 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('.footernav').load('./text/nav_after.html'));
+        } else {
+            // No user is signed in.
+            console.log($('.footernav').load('./text/nav_before.html'));
+
+        }
+    });
+}
+loadSkeleton(); //invoke the function
