300a1004e45dd2250534e9d21140f21488646099
Caroline Su <caroline77899@gmail.com>
2023-11-12 16:51:42 -0800

    update each_info.html

--- scripts/main.js
+++ scripts/main.js
@@ -21,20 +21,20 @@ mapViewButton.addEventListener('click', function () {
 
 // Function to read the quote of the day from Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
-function readMessage(day) {
-    db.collection("testing").doc(day)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
-        .onSnapshot(dayInfo => {                                                               //arrow notation
-            console.log("current document data: " + dayInfo.data().message)
-            quoteOfTheDay = dayInfo.data().message                          //.data() returns data object
-            document.getElementById("quote-goes-here").innerHTML = quoteOfTheDay;      //using javascript to display the data on the right place
+// function readMessage(day) {
+//     db.collection("testing").doc(day)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+//         .onSnapshot(dayInfo => {                                                               //arrow notation
+//             console.log("current document data: " + dayInfo.data().message)
+//             quoteOfTheDay = dayInfo.data().message                          //.data() returns data object
+//             document.getElementById("quote-goes-here").innerHTML = quoteOfTheDay;      //using javascript to display the data on the right place
 
-            //Here are other ways to access key-value data fields
-            //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
-            //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
-            //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
-        })
-}
-readMessage("monday");        //calling the function
+//             //Here are other ways to access key-value data fields
+//             //$('#quote-goes-here').text(dayDoc.data().quote);         //using jquery object dot notation
+//             //$("#quote-goes-here").text(dayDoc.data()["quote"]);      //using json object indexing
+//             //document.querySelector("#quote-goes-here").innerHTML = dayDoc.data().quote;
+//         })
+// }
+// readMessage("monday");        //calling the function
 
 
 
