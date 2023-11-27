99dcd45833084dd7488d049bd1a578f90d3a6703
yuezhang27 <yzhanged@gmail.com>
2023-11-26 19:20:10 -0800

    delete code

--- scripts/main.js
+++ scripts/main.js
@@ -435,25 +435,6 @@ function writeResources(){
 }
 
 
-
-// Time Stamp to xx h: xx min : xx s
-// function formatTimestamp(timestamp) {
-//     const fullTimestamp = timestamp.seconds + timestamp.nanoseconds / 1e9;
-//     const date = new Date(fullTimestamp * 1000);
-
-//     const year = date.getFullYear();
-//     const month = (date.getMonth() + 1).toString().padStart(2, '0');
-//     const day = date.getDate().toString().padStart(2, '0');
-//     const hours = date.getHours().toString().padStart(2, '0');
-//     const minutes = date.getMinutes().toString().padStart(2, '0');
-//     const seconds = date.getSeconds().toString().padStart(2, '0');
-
-//     const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-
-//     return formattedDateTime;
-// }
-
-
 function calculateCountdown(openTimeStr, closeTimeStr) {
     if (openTimeStr ==="0:00"&& closeTimeStr==="24:00" ){
         return 'Opening | 24hours';
