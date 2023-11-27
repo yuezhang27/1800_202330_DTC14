61e2b014d1c9bb649916e40d8a9255098185671f
yuezhang27 <yzhanged@gmail.com>
2023-11-26 19:24:39 -0800

    fix bookmark time

--- scripts/bookmark.js
+++ scripts/bookmark.js
@@ -31,21 +31,37 @@ function insertNameFromFirestore(user) {
 // and retrieves the "saved" array (of bookmarks) 
 // and dynamically displays them in the gallery
 //----------------------------------------------------------
-function formatTimestamp(timestamp) {
-    const fullTimestamp = timestamp.seconds + timestamp.nanoseconds / 1e9;
-    const date = new Date(fullTimestamp * 1000);
 
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    const hours = date.getHours().toString().padStart(2, '0');
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
-
-    const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-
-    return formattedDateTime;
+function calculateCountdown(openTimeStr, closeTimeStr) {
+    if (openTimeStr ==="0:00"&& closeTimeStr==="24:00" ){
+        return 'Opening | 24hours';
+    }
+    let now = new Date();
+    let openTime = new Date(now.toDateString() + ' ' + openTimeStr);
+    let closeTime = new Date(now.toDateString() + ' ' + closeTimeStr);
+    if (openTime > closeTime) {
+        closeTime.setDate(closeTime.getDate() + 1);
+    }
+    if (now < openTime) {
+        return 'Closed | Next Open: In ' + timeDifference(now, openTime);
+    } else if (now >= openTime && now < closeTime) {
+        //opening
+        return 'Is Opening | Close In: ' + timeDifference(now, closeTime);
+    } else {
+        let tomorrowOpen = new Date(openTime);
+        tomorrowOpen.setDate(tomorrowOpen.getDate() + 1);
+        return 'Closed | Next Open: In ' + timeDifference(now, tomorrowOpen);
+    }
 }
+
+
+function timeDifference(start, end) {
+    let diff = end - start;
+    let hours = Math.floor(diff / 3600000);
+    let minutes = Math.floor((diff % 3600000) / 60000);
+    return `${hours} hrs ${minutes} mins`;
+}
+
 
 function getBookmarks(user) {
     db.collection("users").doc(user.uid).get()
@@ -68,15 +84,16 @@ function getBookmarks(user) {
                 db.collection("resources").doc(thisResourceID).get().then(doc => {
                     var title = doc.data().name;       // get value of the "name" key
                     var description = doc.data().description;  // get value of the "description" key
-                    var updateTime = doc.data().last_updated;
-                    realTime = formatTimestamp(updateTime);
+                    var openTime = doc.data().openTime;
+                    var closeTime = doc.data().closeTime;
+                    var countDownText = calculateCountdown(openTime, closeTime)
                     var resourceCode = doc.data().code;    //get ID to each resource to be used for fetching right image
                     var docID = doc.id;
                     let newcard = newcardTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
     
                     //update title and text and image
                     newcard.querySelector('.card-title').innerHTML = title;
-                    newcard.querySelector('.text-muted').innerHTML = "Last update: " + realTime;
+                    newcard.querySelector('.text-muted').innerHTML = countDownText;
                     newcard.querySelector('.description').innerHTML = description;
                     newcard.querySelector('.card-img-bottom').src = `./images/${resourceCode}.jpg`; //Example: NV01.jpg
                     newcard.querySelector('a').href = "each_info.html?docID=" + docID;
